69b5877179f0b9d706bc78c90990cd91d8c21175
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-10-10 13:26:12 +0200

    Updated main files to create and run the server

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from copy_static import copy_static_content
+from site_generator import generate_page
 
 
 dir_path_origin = "./static/"
@@ -7,6 +8,7 @@ dir_path_destination = "./public/"
 
 def main():
     copy_static_content(dir_path_origin, dir_path_destination)
+    generate_page("./content/index.md", "template.html", "./public/index.html")
 
 
 main()
