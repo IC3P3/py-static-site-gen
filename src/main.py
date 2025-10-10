4f2177fd2c09828cca967aabbd5ee330c5e3f74f
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-10-10 13:26:13 +0200

    Made site generation recursive

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
+
 from copy_static import copy_static_content
-from site_generator import generate_page
+from site_generator import generate_pages_recursive
 
 
 dir_path_origin = "./static/"
 dir_path_destination = "./public/"
+dir_path_content = "./content/"
+temaplte_path = "./template.html"
 
 
 def main():
     copy_static_content(dir_path_origin, dir_path_destination)
-    generate_page("./content/index.md", "template.html", "./public/index.html")
+    generate_pages_recursive(
+        dir_path_content,
+        temaplte_path,
+        dir_path_destination
+    )
 
 
 main()
--- src/site_generator.py
+++ src/site_generator.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 from block_markdown import markdown_to_html_node
 from extract_data import extract_title
@@ -6,6 +7,10 @@ from extract_data import extract_title
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    dest_path_pathlib = pathlib.Path(dest_path)
+    if not os.path.exists(str(dest_path_pathlib.parent)):
+        os.makedirs(str(dest_path_pathlib.parent))
 
     with open(from_path) as from_file:
         markdown = from_file.read()
@@ -29,3 +34,13 @@ def generate_page(from_path, template_path, dest_path):
     from_file.close()
     template_file.close()
     dest_file.close()
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    entries = os.listdir(dir_path_content)
+
+    for entry in entries:
+        if os.path.isfile(os.path.join(dir_path_content, entry)):
+            generate_page(os.path.join(dir_path_content, entry), template_path, os.path.join(dest_dir_path, entry).replace(".md", ".html"))
+        else:
+            generate_pages_recursive(os.path.join(dir_path_content, entry), template_path, os.path.join(dest_dir_path, entry))
