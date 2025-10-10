4085ff08e2f7f9993a4a29b0e26c22c995696c07
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-10-10 13:26:05 +0200

    Added image and link extraction from markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -22,3 +23,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
         new_nodes.extend(split)
     return new_nodes
+
+def extract_markdown_images(text):
+    images_regex = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    images = re.findall(images_regex, text)
+    return images
+
+def extract_markdown_links(text):
+    links_regex = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    links = re.findall(links_regex, text)
+    return links
